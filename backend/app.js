8e1a57936ec347b9cc8af1afc4167a3a7cb73f93
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-02 17:49:01 +0200

    ajuot des routes

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,29 @@ app.post('/api/stuff', (req, res, next) => {
   .catch(error => res.status(400).json({ error }))
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).render('detail', { thing }))
+    .catch(error => res.status(404).json({ error }));
+});
+
+// la route pour la mis a jour
+app.put('/api/stuff/:id', (req, res) => {
+  Thing.updateOne(
+    { _id: req.params.id },
+    { ...req.body }
+  )
+  .then(() => res.redirect('/'))
+  .catch(error => res.status(400).json({ error }));
+});
+
+// 🔹 PAGE DETAIL
+app.get('/detail/:id', async (req, res) => {
+  const thing = await Thing.findOne({ _id: req.params.id });
+  res.render('detail', { thing });
+});
+
+app.get('/api/stuff', (req, res, next) => {
 Thing.find()
   .then(things => res.status(200).json(things))
   .catch(error => res.status(400).json({ error }));
@@ -60,11 +82,6 @@ Thing.find()
 
 
 
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
 
 
 
