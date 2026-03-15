46f9c102311239c30b612d9eee00f4c34ea6bb63
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-15 03:43:24 +0200

    le fichier qui controle les routes

---
+++ backend/controlers/controler.js
@@ -0,0 +1,55 @@
+const Thing = require('../models/thing'); //exportation du model des donnees
+
+exports.sendLoginPage = (req,res,next)=>{
+  res.status(200).render('posts')
+};
+
+exports.sendHomePage = (req,res,next)=>{
+  res.status(200).render('index');
+};
+
+exports.createThing = (req, res, next) => {
+  delete req.body._id; //supprimer l'id qui vient par defaut avec le body
+  const thing = new Thing({   //on cree un nouvel object Thing avec les donnees du formulaire
+    ...req.body
+  });
+  thing.save() //la... la methode save() nous permet d'enregistrer les donnees 
+  .then(() => res.status(200).redirect('/')) //la redirection apres insertion
+  .catch(error => res.status(400).json({ error }))
+}
+
+exports.findOneThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).render('detail', { thing }))
+    .catch(error => res.status(404).json({ error }));
+}
+
+exports.sendEditPage = (req, res) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).render('edit', { thing }))
+    .catch(error => res.status(404).json({ error }));
+}
+
+exports.sendDetailPage = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).render('detail', { thing }))
+    .catch(error => res.status(404).json({ error }));
+}
+
+exports.updateOneThing = (req, res) => {
+  Thing.updateOne({ _id: req.params.id },{ ...req.body })
+  .then(() => res.redirect('/'))
+  .catch(error => res.status(400).json({ error }));
+}
+
+exports.deleteOneThing = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.redirect('/'))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.findAllThings = (req, res, next) => {
+Thing.find()
+  .then(things => res.status(200).json(things))
+  .catch(error => res.status(400).json({ error }));
+}
