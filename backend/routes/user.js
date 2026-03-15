0b18285bf677062a3bd68c5272f6eb446ee528cf
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-15 03:41:35 +0200

    premier commit

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+module.exports = router; //la on exporte le module express pour pouvoir l'utiliser dans d'autres fichiers dans
+//  la constante router pour qu'il soit accessible das les autres fichiers
