d53f34cc614a7736498da7414d0e4a07e14f5ca7
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-15 04:12:06 +0200

    placer le controleur

--- backend/controlers/user.js
+++ backend/controlers/user.js
@@ -1,8 +1,24 @@
+const bcrypt = require('bcrypt');
 
-exports.singup = (req, res, next) => {
+const User = require('../models/user');
 
-}
+//la fonction signup pour l'enregistrement d'un nouvel utilisateur
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error })
+        );
+};
 
+// la fonction login pour la connexion d'un utilisateur
 exports.login = (req, res, next) => {
-    
+
 }
